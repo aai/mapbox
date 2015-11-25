9e7201bc639a2b383ee299200d1cb03c7a745975
Alecs Popa <alecs.popa@gmail.com>
2015-11-25 17:14:51 +0200

    Refactor require of geojson

--- lib/mapbox.rb
+++ lib/mapbox.rb
@@ -3,9 +3,7 @@ require 'mapbox/mapbox_utils'
 require 'mapbox/abstract_marker'
 require 'mapbox/mapbox_marker'
 require 'mapbox/custom_marker'
-require 'mapbox/geojson/base'
-require 'mapbox/geojson/geometry'
-require 'mapbox/geojson/properties'
+require 'mapbox/geojson'
 require 'mapbox/static_map'
 
 module Mapbox
